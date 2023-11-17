5650be66df6fb972a6792a1d8bda1338fe573027
notS0LD13R <joelsebin@gmail.com>
2023-11-17 13:45:19 +0530

    feat:speakers page added

--- js/commInfo.js
+++ js/commInfo.js
@@ -1,47 +1,47 @@
 const comm = {
-  "Dr. Siddharth Shelly": [
-    "./images/committee/Siddharth Shelly.jpg",
-    "General Chair",
-    "Assistant Professor, Department of Electronics and Communication, MACE",
-  ],
-  "Dr. Varghese Cherian": [
-    "./images/committee/Er. Varghese Cherian.jpg",
-    "Co-General Chair",
-    "IEEE SSIT Kerala Chapter",
-  ],
-  "Dr. Bos Mathew Jos": [
-    "./images/committee/Bos_Mathew.jpg",
-    "TPC Chair",
-    "Professor, Department of Electrical and Electronics Engineering, MACE Principal, Mar Athanasius College of Engineering Advisor, IEEE IAS SBC MACE and IEEE IE/PELS Jt.Chapter MACE",
-  ],
-  "Dr. Kumarvel S": [
-    "./images/committee/Dr. Kumarvel S.png",
-    "TPC Co-Chair",
-    "Department of Electrical Engineering,National Institute of Technology, Calicut, Kerala",
-  ],
-  "Dr. Reenu George": [
-    "./images/committee/Reenu George.jpg",
-    "Publication Chair",
-    "Assistant Professor, Department of Electrical and Electronics Engineering, MACE Advisor, IEEE SPS SBC MACE",
-  ],
-  "Dr. Jaison Mathew": [
-    "./images/committee/Dr. Jaison Mathew .jpg",
-    "Publication Co-Chair",
-    "Convenor, Conference Activities Board, IEEE Kerala Section",
-  ],
-  "Prof. Neethu Salim": [
-    "./images/committee/Neethu salim.jpg",
-    "Publicity chair",
-    "Assistant Professor, Department of Electrical and Electronics Engg., MACE Advisor, IEEE WiE AG MACE",
-  ],
-  "Prof. Neema S": [
-    "./images/committee/neema s .jpg",
-    "Finance Committee Chair",
-    "Department of Electrical and Electronics Engineering, MACE Branch Counselor, IEEE MACE SB",
-  ],
-  "Prof. Nandan S.": [
-    "./images/committee/Prof Nandan S.jpg",
-    "Finance Committee Co-Chair",
-    "Treasurer, IEEE Kerala Section",
-  ],
+    "Dr. Siddharth Shelly": [
+        "./images/committee/Siddharth Shelly.jpg",
+        "General Chair",
+        "Assistant Professor, Department of Electronics and Communication, MACE",
+    ],
+    "Dr. Varghese Cherian": [
+        "./images/committee/Er. Varghese Cherian.jpg",
+        "Co-General Chair",
+        "IEEE SSIT Kerala Chapter",
+    ],
+    "Dr. Bos Mathew Jos": [
+        "./images/committee/Bos_Mathew.jpg",
+        "TPC Chair",
+        "Professor, Department of Electrical and Electronics Engineering, MACE Principal, Mar Athanasius College of Engineering Advisor, IEEE IAS SBC MACE and IEEE IE/PELS Jt.Chapter MACE",
+    ],
+    "Dr. Kumarvel S": [
+        "./images/committee/Dr. Kumarvel S.png",
+        "TPC Co-Chair",
+        "Department of Electrical Engineering,National Institute of Technology, Calicut, Kerala",
+    ],
+    "Dr. Reenu George": [
+        "./images/committee/Reenu George.jpg",
+        "Publication Chair",
+        "Assistant Professor, Department of Electrical and Electronics Engineering, MACE Advisor, IEEE SPS SBC MACE",
+    ],
+    "Dr. Jaison Mathew": [
+        "./images/committee/Dr. Jaison Mathew .jpg",
+        "Publication Co-Chair",
+        "Convenor, Conference Activities Board, IEEE Kerala Section",
+    ],
+    "Prof. Neethu Salim": [
+        "./images/committee/Neethu salim.jpg",
+        "Publicity chair",
+        "Assistant Professor, Department of Electrical and Electronics Engg., MACE Advisor, IEEE WiE AG MACE",
+    ],
+    "Prof. Neema S": [
+        "./images/committee/neema s .jpg",
+        "Finance Committee Chair",
+        "Department of Electrical and Electronics Engineering, MACE Branch Counselor, IEEE MACE SB",
+    ],
+    "Prof. Nandan S.": [
+        "./images/committee/Prof Nandan S.jpg",
+        "Finance Committee Co-Chair",
+        "Treasurer, IEEE Kerala Section",
+    ],
 };
