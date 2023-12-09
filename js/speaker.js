1c0975711f6862f311c184149a99c5ebf65150c3
notS0LD13R <joelsebin@gmail.com>
2023-12-09 23:24:56 +0530

    feat:link in cards

--- js/speaker.js
+++ js/speaker.js
@@ -1,33 +1,40 @@
 const commContainer = document.querySelector(".committee__cards");
 const makeCardSpeaker = (name, arr) => {
-  const a = document.createElement("a");
-  if (arr[3]) a.href = arr[3];
-  const card = document.createElement("div");
-  card.classList.add("committee__card");
+    // const a = document.createElement("a");
+    // if (arr[3]) a.href = arr[3];
+    const card = document.createElement("div");
+    card.classList.add("committee__card");
 
-  const commImg = document.createElement("img");
-  commImg.classList.add("committee__img");
-  commImg.src = arr[0];
+    const commImg = document.createElement("img");
+    commImg.classList.add("committee__img");
+    commImg.src = arr[0];
 
-  const commText = document.createElement("div");
-  commText.classList.add("committee__text");
+    const commText = document.createElement("div");
+    commText.classList.add("committee__text");
 
-  const commName = document.createElement("p");
-  commName.innerText = name;
+    const commName = document.createElement("p");
+    commName.innerText = name;
 
-  const commDesignation = document.createElement("p");
-  commDesignation.classList.add("committee__designation");
-  commDesignation.innerHTML = `${arr[1]} <br> ${arr[2]}`;
-  commDesignation.title = arr[2];
+    const commDesignation = document.createElement("p");
+    commDesignation.classList.add("committee__designation");
+    commDesignation.innerHTML = `${arr[1]} <br> ${arr[2]}`;
+    commDesignation.title = arr[2];
 
-  commText.append(commName);
-  commText.append(commDesignation);
-  card.append(commImg);
-  card.append(commText);
-  a.append(card);
-  commContainer.append(a);
+
+      const redirectButton = document.createElement('a');
+      redirectButton.href=arr[3]
+      redirectButton.innerHTML=`<button class="know_more">
+         <img src="https://static.thenounproject.com/png/640392-200.png" alt="">
+       </button>`
+    commText.append(commName);
+    commText.append(commDesignation);
+    card.append(commImg);
+    card.append(commText);
+    card.append(redirectButton)
+    // a.append(card);
+    commContainer.append(card);
 };
 
 for (k in speakers) {
-  makeCardSpeaker(k, speakers[k]);
+    makeCardSpeaker(k, speakers[k]);
 }
