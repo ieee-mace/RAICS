bdd32c181f2c8595250b7d7f9f6d9ce71b4548c8
Goutham K B <goutham79007@gmail.com>
2023-12-12 18:26:35 +0530

    description update

--- js/speakerInfo.js
+++ js/speakerInfo.js
@@ -1,26 +1,26 @@
 const speakers = {
-    "Dr. Jobin Francis": [
-        "./images/speakers/Jobin.png",
-        "Assistant Professor <br> Electrical Engineering, IIT Palakkad",
-        "",
-        "https://iitpkd.ac.in/people/jobinfrancis",
-    ],
-    "Dr. C. S. Shankar Ram": [
-        "./images/speakers/Shankar.png",
-        "Professor <br> Department of Engineering Design, IIT Madras",
-        "",
-        "https://ed.iitm.ac.in/~shankarram",
-    ],
-    "Dr. Jimson Mathew": [
-        "./images/speakers/Jimson.jpg",
-        "Associate Professor <br> Head  Department of Computer Science & Engineering",
-        "",
-        "",
-    ],
-    "Eby K. Kuriakose": [
-        "./images/speakers/Eby.jpg",
-        "Senior IT Professional <br> Amazon Web Services",
-        "",
-        "https://www.linkedin.com/in/ebykkuriakose/",
-    ],
+  "Dr. Jobin Francis": [
+    "./images/speakers/Jobin.png",
+    "Assistant Professor, <br> Electrical Engineering, IIT Palakkad",
+    "",
+    "https://iitpkd.ac.in/people/jobinfrancis",
+  ],
+  "Dr. C. S. Shankar Ram": [
+    "./images/speakers/Shankar.png",
+    "Professor, <br> Department of Engineering Design, IIT Madras",
+    "",
+    "https://ed.iitm.ac.in/~shankarram",
+  ],
+  "Dr. Jimson Mathew": [
+    "./images/speakers/Jimson.jpg",
+    "Associate Professor <br> Head, Department of Computer Science & Engineering, IIT Patna",
+    "",
+    "",
+  ],
+  "Eby K. Kuriakose": [
+    "./images/speakers/Eby.jpg",
+    "Senior IT Professional, <br> Amazon Web Services",
+    "",
+    "https://www.linkedin.com/in/ebykkuriakose/",
+  ],
 };
