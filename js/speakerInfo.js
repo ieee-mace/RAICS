1c4b757683fc03a3de36a0de1721838a341c4dfd
Goutham K B <goutham79007@gmail.com>
2024-03-21 00:09:31 +0530

    redesigned speakers page css js and html

--- js/speakerInfo.js
+++ js/speakerInfo.js
@@ -1,38 +1,128 @@
-const speakers = {
+// const speakers = {
+// "Dr. C. S. Shankar Ram": [
+//   "./images/speakers/Shankar.png",
+//   "Professor, <br> Department of Engineering Design, IIT Madras",
+//   "",
+//   "https://ed.iitm.ac.in/~shankarram",
+// ],
+//   "Dr. Jimson Mathew": [
+//     "./images/speakers/Jimson.jpg",
+//     "Associate Professor <br> Head, Department of Computer Science & Engineering, IIT Patna",
+//     "",
+//     "https://www.linkedin.com/in/dr-jimson-mathew-1511a816?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+//   ],
+//   "Eby K. Kuriakose": [
+//     "./images/speakers/Eby.jpg",
+//     "Senior IT Professional, <br> Amazon Web Services",
+//     "",
+//     "https://www.linkedin.com/in/ebykkuriakose/",
+//   ],
+//   "Dr. Jobin Francis": [
+//     "./images/speakers/Jobin.png",
+//     "Assistant Professor, <br> Electrical Engineering, IIT Palakkad",
+//     "",
+//     "https://iitpkd.ac.in/people/jobinfrancis",
+//   ],
+//   "Dr. Jayakumar M ": [
+//     "./images/speakers/Jayakumar1.jpg",
+//     "Project Director PSLV, ISRO",
+//     "",
+//     "",
+//   ],
+//   "Dr. Shriram Kris Vasudevan": [
+//     "./images/speakers/Shriram.jpeg",
+//     "Lead Technical Evangelist,<br> Asia Pacific and Japan,<br> Intel Technology India Pvt. Ltd",
+//     "",
+//     "",
+//   ],
+// };
+
+const speakerInfo = {
+  "Dr. Shriram Kris Vasudevan": [
+    "./images/speakers/Shriram.jpeg",
+    "https://www.linkedin.com/in/shriramkvasudevan",
+    "GenAI – The Tech Revolution",
+    `Dr. Shriram Kris Vasudevan has over 17 years of experience in 
+    the Industry and Academia together. He holds a Doctorate in embedded systems. He has
+    authored / co-authored 45 books for various publishers including Taylor and Francis,
+    Oxford University Press, and Wiley. He also has been granted 13 patents so far.
+    Shriram is a hackathon enthusiast and has been awarded by Harvard University,
+    AICITE, CII, Google, TDRA Dubai, Govt. Of Saudi Arabia, Govt. Of India and a lot
+    more. He has published more than 150 research articles. He was associated with L&T
+    Technology Services before joining in current role with a intel. Shriram Vasudevan
+    runs a YouTube channel in his name which has more than 43k subscribers and maintains
+    a wide range of playlists on varied topics. Dr. Shriram is a public speaker too. He
+    is oneAPI certified Instructor, Intel oneAPI certified Instructor, Google Cloud
+    Ambassador, Streamlit Education Ambassador, AWS Ambassador, ACM Distinguished
+    Speaker and NASSCOM Prime Ambassador. Shriram is a Fellow IEI, Fellow IETE and
+    Senior Member IEEE.<br>
+    <br>
+    1. What is oneAPI?<br>
+    <br>
+    2. Developer Challenges – How to overcome?<br>
+    <br>
+    3. Intel Developer Cloud – World of learning starts here.<br>
+    <br>
+    4. AI with Intel Developer cloud (IDC)<br>
+    <br>
+    5. GenAI introduction, prompt engineering guidelines, GenAI with IDC (Text to Image,
+    Image to Image, and more examples with Stable diffusion)<br>
+    <br>
+    6. Tools for code productivity (Vtune profiler, Advisor, CUDA to SYCL conversion)<br>
+    <br>
+
+    Brief Overview <br>
+    <br>
+    What is oneAPI?<br>
+    <br>
+    oneAPI is an open, standards-based, unified cross-architecture programming model
+    that provides developers the freedom to choose the best hardware (CPU, GPU and
+    FPGAs) for accelerated computing. oneAPI delivers performance without proprietary
+    lock-in while interoperating with code written in familiar standards including C,
+    C++, SYCL, Fortran, Python, MPI and OpenMP.With oneAPI, developers can choose the
+    best architecture for their specific problem without needing to completely rewrite
+    software for the next architecture and platform. <br>
+    <br>
+    Intel®️ oneAPI Toolkits– Are built on Intel’s rich heritage of proven CPU developer
+    tools now expanded for XPUs; they support familiar languages and standards while
+    providing full interoperability with existing code. The toolkits deliver on the
+    performance potential of Intel®️ architectures and lower software development and
+    maintenance costs. oneAPI and Intel oneAPI Toolkits together bring productive and
+    performant heterogenous programming to developers. <br>
+    <br>
+    Intel Developer Cloud – It is a sandbox enabling the users to wonderful stuff with
+    amount of facilities and guidelines it provides. Access to the Intel Developer Cloud
+    can be introduced for the participants and they can learn Machine learning, GenAI
+    and SYCL (HPC) through the Intel Developer Cloud.`,
+  ],
   "Dr. C. S. Shankar Ram": [
     "./images/speakers/Shankar.png",
-    "Professor, <br> Department of Engineering Design, IIT Madras",
-    "",
     "https://ed.iitm.ac.in/~shankarram",
+    "Topic to be Announced",
+    "",
   ],
   "Dr. Jimson Mathew": [
     "./images/speakers/Jimson.jpg",
-    "Associate Professor <br> Head, Department of Computer Science & Engineering, IIT Patna",
-    "",
     "https://www.linkedin.com/in/dr-jimson-mathew-1511a816?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    "Topic to be Announced",
+    "",
   ],
   "Eby K. Kuriakose": [
     "./images/speakers/Eby.jpg",
-    "Senior IT Professional, <br> Amazon Web Services",
-    "",
     "https://www.linkedin.com/in/ebykkuriakose/",
+    "Topic to be Announced",
+    "",
   ],
   "Dr. Jobin Francis": [
     "./images/speakers/Jobin.png",
-    "Assistant Professor, <br> Electrical Engineering, IIT Palakkad",
-    "",
     "https://iitpkd.ac.in/people/jobinfrancis",
+    "Topic to be Announced",
+    "",
   ],
   "Dr. Jayakumar M ": [
     "./images/speakers/Jayakumar1.jpg",
-    "Project Director PSLV, ISRO",
     "",
-    "",
-  ],
-  "Dr. Shriram Kris Vasudevan": [
-    "./images/speakers/Shriram.jpeg",
-    "Lead Technical Evangelist,<br> Asia Pacific and Japan,<br> Intel Technology India Pvt. Ltd",
-    "",
+    "Topic to be Announced",
     "",
   ],
 };
